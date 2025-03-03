6c52181032280ebb36eb1081e535c09630cf5579
Arkhyp Vasylenko <vasilenko.arhip@gmail.com>
2025-03-03 09:15:16 +0000

    Change name of endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,6 @@
-from api.routers.auth import router as auth_router
-from api.routers.users import router as users_router
-from api.routers.dialogs import router as dialog_router
+from api.endpoints.auth import router as auth_router
+from api.endpoints.users import router as users_router
+from api.endpoints.dialogs import router as dialog_router
 
 
 all_routes = [
